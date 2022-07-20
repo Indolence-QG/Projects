db6c38cd8be3fd9f596dbba46401e8fe345581b6
穷鬼 <indolence408@163.com>
2022-07-20 15:56:56 +0800

    加一句helloworld

--- spring_boot_1/src/main/java/demo/SpringBoot1Application.java
+++ spring_boot_1/src/main/java/demo/SpringBoot1Application.java
@@ -9,6 +9,7 @@ public class SpringBoot1Application {
     public static void main(String[] args) {
 
         System.out.println("hello");
+        System.out.println("hello");
         SpringApplication.run(SpringBoot1Application.class, args);
     }
 
