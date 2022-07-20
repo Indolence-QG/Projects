7d6cf50e4ba674c00dd935b265a2749d1cf637f8
穷鬼 <indolence408@163.com>
2022-07-20 15:51:46 +0800

    “springboot1提交”

---
+++ spring_boot_1/src/main/java/demo/SpringBoot1Application.java
@@ -0,0 +1,15 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot1Application {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello");
+        SpringApplication.run(SpringBoot1Application.class, args);
+    }
+
+}
