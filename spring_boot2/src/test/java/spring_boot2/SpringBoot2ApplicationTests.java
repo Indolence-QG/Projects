a7c55376676b291de767fed7e6f3fd2f7f727f95
穷鬼 <indolence408@163.com>
2022-07-20 16:11:13 +0800

    创建项目springboot2

---
+++ spring_boot2/src/test/java/spring_boot2/SpringBoot2ApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_boot2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
